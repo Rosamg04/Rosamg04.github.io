f2bd1f5bce45b4fc33ffaa922ef29fded5f76933
Rosamg04 <96417254+Rosamg04@users.noreply.github.com>
2022-04-20 07:01:18 +0000

    añadir ejercicio 3

---
+++ ejercicios-javascript/js-ejerc03-operadores-binarios.js
@@ -0,0 +1,15 @@
+var A= 10;
+var B= 20;
+var multiplica= A*B;
+var divide= A/B;
+var suma= A+B;
+var resta= A-B;
+A==B;
+document.write("<p>El resultado de dividir ",A," entre ",B, " es ",divide, "</p>"); 
+document.write("<p>El resultado de multiplicar ",A," por ",B, " es ",multiplica, "</p>"); 
+document.write("<p>El resultado de sumar ",A," más ",B, " es ",suma, "</p>"); 
+document.write("<p>El resultado de restar ",A," menos ",B, " es ",resta, "</p>"); 
+document.write("<p> El resultado de evaluar si ",A," y ",B, " son iguales es: ",A==B,"</p>"); 
+document.write("<p> El resultado de evaluar si ",A," y ",B, " son diferentes es: ",A!=B,"</p>"); 
+document.write("<p> El resultado de evaluar si ",A," es menor  que ",B, "  es: ",A<B,"</p>"); 
+document.write("<p> El resultado de evaluar si ",A," es mayor de  ",B, "  es: ",A>B,"</p>"); 
