40a2863fa463a7b476f0b0feb6faa2ba576171e7
Rosamg04 <96417254+Rosamg04@users.noreply.github.com>
2022-05-04 07:09:35 +0000

    añadir ejercicio 05

---
+++ ejercicios-javascript/js-ejerc05-cadenas-de-caracteres.js
@@ -0,0 +1,5 @@
+var nombre="Rosa"
+var apellidos= "Menéndez García"
+var primera_letra= nombre[0]
+document.write("<p>Las variables nombre y apellidos encadenadas resultan ",nombre," ",apellidos,"</p>")
+document.write("<p>La primera letra de mi nombre es ", primera_letra,"</p>")
