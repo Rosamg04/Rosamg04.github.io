3830820d3b02d5b0c89fb5d00af230e25c1aee7c
Rosamg04 <96417254+Rosamg04@users.noreply.github.com>
2022-05-09 09:19:04 +0000

    Añadir examen

---
+++ ejercicios-javascript/examen_final.js
@@ -0,0 +1,6 @@
+alert("Bienvenido al examen final")
+var nombre=prompt("Introduzca su nombre")
+alert("Hola " +nombre)
+if(nombre[0]== "a"||nombre[0]== "e"||nombre[0]== "i"||nombre[0]== "o"||nombre[0]== "u"){document.write("<p>La primera letra de tu nombre es una vocal</p>")}
+else
+{document.write("La primera letra de tu nombre es una consonante")}
