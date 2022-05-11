725315df485cd711a9f69b0e9251cb1e244e0d30
Rosamg04 <96417254+Rosamg04@users.noreply.github.com>
2022-05-11 07:20:23 +0000

    añadir ejercicio 7

---
+++ ejercicios-javascript/js-ejerc07-estructuras.js
@@ -0,0 +1,34 @@
+var culpable = window.prompt("¿Eres culpable?"); // si, no 
+
+  
+
+if(culpable=="si"){ 
+
+document.write("<p>Irás a la cárcel</p>"); 
+
+}else if(culpable=="no"){ 
+
+document.write("<p>Irás a casa</p>"); 
+
+}else{ 
+
+document.write("<p>La documentación por favor</p>"); 
+
+} 
+var comprar = window.prompt("¿Te gusta esta chaqueta?"); // si, no 
+
+  
+
+if(comprar=="si"){ 
+
+document.write("<p>Yo te la compro</p>"); 
+
+}else if(comprar=="no"){ 
+
+document.write("<p>Puedes buscar otra</p>"); 
+
+}else{ 
+
+document.write("<p>Sino buscamos otra cosa</p>"); 
+
+} 
